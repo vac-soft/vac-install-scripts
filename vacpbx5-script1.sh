ea539f24df47578fedc2ecfe9f9bd10012f9d0b4
Kirubhakaran <57623979+dkkaran1996@users.noreply.github.com>
2025-07-21 19:55:41 +0530

    Create vacpbx5-script1.sh

---
+++ vacpbx5-script1.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Welcome to VAC PBX Gen 4 Installation"
+echo "--------------------------------------"
+echo "This Version is v5.1"
+echo "--------------------------------------"
+echo "You are in level one."
+echo "--------------------------------------"
+
+# Set timezone
+echo "Setting timezone to Asia/Kolkata..."
+timedatectl set-timezone Asia/Kolkata
+
+# Update system packages
+echo "Checking for updates..."
+yum check-update
+
+echo "Launching updates..."
+yum update -y
+
+# Install epel-release
+echo "Installing epel-release..."
+yum -y install epel-release
+
+# Update again after installing epel-release
+echo "Updating system again..."
+yum update -y
+
+# Disable SELinux
+echo "Disabling SELinux..."
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+
+# Reboot the system
+echo "Rebooting the system. Hold on..."
+reboot
